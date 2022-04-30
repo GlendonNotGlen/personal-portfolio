ef428fb9175bf7422460e1e13ba208125aa357a4
GlendonNotGlen <glendonng38@gmail.com>
2022-04-30 14:22:46 +0800

    Add prototype 2 for contact form

--- contact.php
+++ contact.php
@@ -33,7 +33,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: https://glendonng.netlify.app/');
+header('Location: https://glendonng.netlify.app/index.html');
 
 
 // Function to validate against any email injection attempts
